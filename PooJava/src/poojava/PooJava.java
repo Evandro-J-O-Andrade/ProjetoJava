ef187dee29297b87888402c3b594f488c1bbaf20
Evandro Andrade <evandro_j.o.a@hotmail.com>
2024-08-20 05:32:02 -0300

    Codando mais umas

--- PooJava/src/poojava/PooJava.java
+++ PooJava/src/poojava/PooJava.java
@@ -12,11 +12,20 @@ public class PooJava {
      */
     public static void main(String[] args) {
         Caneta c1 = new Caneta();
+        c1.modelo ="Bick";
         c1.cor = "Azul";
-        c1.ponta =0.5f;
+        c1.ponta = 0.5f;
         c1.destampar();
         c1.status();
         c1.rabiscar();
+        
+        Caneta c2 = new Caneta();
+        c2.modelo = "Black";
+        c2.cor ="Preta";
+        c2.ponta = 0.7f; 
+        c2.tampar();
+        c2.status();
+        c2.rabiscar();
     }
     
 }
